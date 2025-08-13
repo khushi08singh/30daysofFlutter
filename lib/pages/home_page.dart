fde9689a1519365c91e542396145a5398a63b89d
khushi08singh <08singhkhushi11@gmail.com>
2025-08-13 10:38:36 +0530

    day 3 completed

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 31;
+    final String name = "khushi";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name !!"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
